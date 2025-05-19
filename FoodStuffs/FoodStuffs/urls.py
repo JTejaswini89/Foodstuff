3f7f3cdeaa684a3a6586268221a3661b95abd728
Tejaswini <tejaswinijonnagaddala@gmail.com>
2025-05-19 20:30:51 +0530

    Add FoodStuffs1 folder to repository

---
+++ FoodStuffs/FoodStuffs/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for FoodStuffs project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from User import views
+from custom_admin import views as vi
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name='login_user'),       # match renamed view
+    path('register/', views.register, name='register_user'),  # match renamed view
+    path('main/', views.main_page, name='main'),
+    path('learn-more/', views.learnmore, name='learnmore'),
+    path('logout/', views.logout_user, name='logout_user'),
+
+path('custom_admin/', vi.custom_admin_home, name='custom_admin_home'),
+path('admin_dashboard/', vi.admin_dashboard, name='admin_dashboard'),  # ✅ added name
+]
+
+
